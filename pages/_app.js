a9c531b778a86ba13d75fa15edfc9a3c1dfea2bf
Samuel Rodriguez <samuelyret@gmail.com>
2023-11-08 12:26:41 +0100

    Add starting code from the course.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import '../styles/bootstrap.min.css';
+import '../styles/site.css';
+import '../styles/fontawesome/css/all.css';
+import '../styles/poppins/poppins.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
