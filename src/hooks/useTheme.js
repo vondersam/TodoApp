8bbc5d73eb4e19e22731d6bae9044bb65ba84d75
Samuel Rodriguez <samuelyret@gmail.com>
2023-11-09 13:21:35 +0100

    useDebug value in a custom hook to debug state with a function as second parameter.

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,7 +2,13 @@ import { useState, useDebugValue } from 'react';
 
 export default function useTheme() {
   const [darkTheme, setDarkTheme] = useState(false);
-  useDebugValue(`theme ${darkTheme ? 'dark' : 'light'}`);
+  useDebugValue(`${darkTheme ? 'dark' : 'light'}`, (val) => {
+    if (val == 'dark') {
+      return 'The theme is dark';
+    } else {
+      return 'The theme is light';
+    }
+  });
   const toggleTheme = () => setDarkTheme(!darkTheme);
   return { darkTheme, toggleTheme };
 }
